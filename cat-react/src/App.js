1ca3ff2a07aa681a753fd68bbac0febcba7a69c2
rBabett <richterbabett03@gmail.com>
2023-01-24 13:41:55 +0100

    add div that shows which button is active

--- cat-react/src/App.js
+++ cat-react/src/App.js
@@ -6,9 +6,6 @@ import { useState } from 'react';
 
 function App() {
   const [ActivePage, setActivePage] = useState('Home');
-  if (ActivePage === 'Gallery') {
-    console.log('Gallery')
-  }
   
   return (
     <div>
@@ -16,8 +13,11 @@ function App() {
         <Navbar handleClick={setActivePage}/>
       </div>
       <div id="page">
-        {(ActivePage === 'Gallery') ?
-          (<div>Hello</div>) : (<div>Nem hello</div>)
+        {(ActivePage === 'About') ? (<div>About component</div>) 
+        : (ActivePage === 'Donate') ? (<div>Donate component</div>)
+        : (ActivePage === 'Gallery') ? (<div>Gallery component</div>)
+        : (ActivePage === 'Contact') ? (<div>Contact component</div>)
+        : (<div>Home</div>)
         }
       </div>
     </div>
