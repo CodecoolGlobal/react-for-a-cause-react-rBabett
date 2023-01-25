7466b1ed616d916f2a30e4cfd2c98a1da3ec56fc
rBabett <richterbabett03@gmail.com>
2023-01-25 15:03:21 +0100

    add background to articles and shadow to titles

--- cat-react/src/components/About.js
+++ cat-react/src/components/About.js
@@ -1,20 +1,20 @@
 function About() {
   return(
     <div id="about_component">
-      <h1>Cute Animal Tending</h1>
+      <h1 className="titles">Cute Animal Tending</h1>
       <p>Dedicated to the well-being of cute animals, the CAT leads and champions global efforts to give each and every cute animal, anywhere on the planet an equal chance to live a healthy life and be in caring hands.</p>
       <br></br>
 
-      <h2>Who We Are</h2>
+      <h2 className="titles">Who We Are</h2>
       <p>Founded in 1989, CAT is the United Nations agency that connects nations, partners and people to rescue cute animals, keep the world safe for them and serve the vulnerable – so every cute animal, everywhere can attain the highest level of health and care.
       As the world’s leading conservation organization, CAT works in nearly 100 countries. At every level, we collaborate with people around the world to develop and deliver innovative solutions that protect communities, wildlife, and the places in which they live.</p> 
       <br></br>
 
-      <h2>What We Do</h2>
+      <h2 className="titles">What We Do</h2>
       <p>CAT promotes healthier lives for the cute animals – from pregnancy care through old age. We lead shelters all around the world where we care for animals we rescued and ensure they will find a better home.</p>
       <br></br>
 
-      <h2>Our Values</h2>
+      <h2 className="titles">Our Values</h2>
       <p>We bear witness to the accelerating decline of nature and our climate, with serious consequences for life on Earth, including our own.
         <br></br>
         Our vision is to build a future in which people live in harmony with nature. To deliver this mission, we work to conserve and restore biodiversity, the web that supports all life on Earth; to reduce humanity’s environmental footprint; and to ensure the sustainable use of natural resources to support current and future generations.
@@ -27,16 +27,16 @@ function About() {
         <b>Collaboration</b>: We deliver impact at the scale of the challenges we face through the power of collective action and innovation.</p>
       <br></br>
 
-      <h2>Ethics</h2>
+      <h2 className="titles">Ethics</h2>
       <p>CAT is committed to a culture which allows people to safely raise concerns of inappropriate conduct by employees of CAT, its partners, and people associated with CAT, without fear of reprisals. Likewise, local communities or individuals from those communities who believe they are negatively impacted by CAT’s activities are welcome to send their concerns. Complaints may be submitted in the complainant’s native language, to the local CAT office or to our EthicsPoint reporting line.
       </p><br></br>
 
-      <h2>Commitment To Integrity And Good Conduct
+      <h2 className="titles">Commitment To Integrity And Good Conduct
       </h2>
       <p>CAT commits to the highest standards of professionalism, integrity and ethics in our workplace and in our activities. These commitments are fundamental to creating effective, lasting and equitable solutions to today’s environmental challenges.
       </p><br></br>
 
-      <h2>Commitment To Independence</h2>
+      <h2 className="titles">Commitment To Independence</h2>
       <p>World Wildlife Fund, Inc. (CAT-US) is a U.S. public charity whose mission is to leverage sound science to conserve nature and reduce the most pressing threats to the diversity of life on Earth. We are an independent organization overseen by a Board of Directors with expertise in science, conservation, finance, and public policy, who volunteer their time and service.
       </p>
       <p>All actions, undertakings, and positions of CAT-US are determined by the organization alone. CAT does not operate as an agent or representative for any funder and does not accept funding from any source that could compromise our independence.
@@ -45,7 +45,7 @@ function About() {
       </p><br></br>
 
 
-      <h2>How We Work</h2>
+      <h2 className="titles">How We Work</h2>
       <p>For Nature, For People, Forever
         CAT works to help local communities conserve the natural resources they depend upon; transform markets and policies toward sustainability; and protect and restore species and their habitats. Our efforts ensure that the value of nature is reflected in decision-making from a local to a global scale.
         <br></br>CAT connects cutting-edge conservation science with the collective power of our partners in the field, more than 1.3 million supporters in the United States and 5 million globally, and our partnerships with communities, companies, and governments.
